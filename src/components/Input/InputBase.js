bbfc255f211d5ee3c8865cfc6e2844116334ff42
KRASAVA <vladimirova.a-a@yandex.ru>
2021-10-10 23:20:57 +0400

    Add error message

--- src/components/Input/InputBase.js
+++ src/components/Input/InputBase.js
@@ -5,14 +5,17 @@ import styles from './Input.module.scss';
 
 const InputBase = ({value, handleChange, placeholder, width, className, error, ...attributes}) => {
 	return (
-		<input
-			className={classNames(styles.inputField, styles[className], {[styles.error]: error})}
-			style={{width: width}}
-			value={value}
-			onChange={(event) => handleChange(event.target.value)}
-			placeholder={placeholder}
-			{...attributes}
-		/>
+		<>
+			<input
+				className={classNames(styles.inputField, styles[className], {[styles.error]: !!error})}
+				style={{width: width}}
+				value={value}
+				onChange={(event) => handleChange(event.target.value)}
+				placeholder={placeholder}
+				{...attributes}
+			/>
+			{error && <span className={styles.errorText}>{error}</span>}
+		</>
 	);
 };
 
