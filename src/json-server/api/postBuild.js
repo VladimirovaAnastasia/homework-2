4fbe101e7a314de4e0de9f6e54e213d99058da16
KRASAVA <vladimirova.a-a@yandex.ru>
2021-10-11 00:12:35 +0400

    Add date and time for build run

--- src/json-server/api/postBuild.js
+++ src/json-server/api/postBuild.js
@@ -1,13 +1,23 @@
 const mockStore = require('../mock-store');
 const buildsTemplate = require('./../data/build');
+const statuses = require('./../data/status');
+const format = require('date-fns/format');
+const ruLocale = require('date-fns/locale/ru');
 
 const postBuild = (req) => {
 	const hash = req.body.hash;
 
 	mockStore.setLastBuildNumber();
 
+	const date = new Date();
+	const formatDate = format(date, 'd LLL, kk:mm', {locale: ruLocale});
+
+	const randomStatusIndex = Math.floor(Math.random() * statuses.length);
+	
 	const build = {
 		...buildsTemplate,
+		status: statuses[randomStatusIndex],
+		date: formatDate.replace('.', ''),
 		hash,
 		number: mockStore.lastBuildNumber,
 	};
