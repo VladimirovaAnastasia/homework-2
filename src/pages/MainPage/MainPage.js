be25b085f196e7f6090a6474e95bc963082d76b3
KRASAVA <vladimirova.a-a@yandex.ru>
2021-10-12 01:02:17 +0400

    Import builds from const.js

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -8,48 +8,9 @@ import {ReactComponent as BuildSvg} from '@/assets/icons/play.svg';
 import {Header} from '@/components/Header';
 import {Modal} from '@/components/Modal';
 import {ThemeSwitcher} from '@/components/ThemeSwitcher';
+import {BUILDS} from '@/const';
 
 import styles from './MainPage.module.scss';
-
-
-const buildCards = [
-	{
-		status: 'fail',
-	},
-	{
-		status: 'done',
-	},
-	{
-		status: 'wait',
-	},
-	{
-		status: 'fail',
-	},
-	{
-		status: 'done',
-	},
-	{
-		status: 'wait',
-	},
-	{
-		status: 'fail',
-	},
-	{
-		status: 'done',
-	},
-	{
-		status: 'wait',
-	},
-	{
-		status: 'fail',
-	},
-	{
-		status: 'done',
-	},
-	{
-		status: 'wait',
-	},
-];
 
 const MainPage = () => {
 	const history = useHistory();
@@ -126,11 +87,11 @@ const MainPage = () => {
 
 			{isSettings ? (
 				<div className={styles.mainPage}>
-					{buildCards.map(
+					{BUILDS.map(
 						(buildCard, index) =>
-							index < showedCardsCount && <BuildCard key={buildCard + index} status={buildCard.status} />
+							index < showedCardsCount && <BuildCard key={buildCard + index} build={buildCard} />
 					)}
-					{buildCards.length > showedCardsCount && (
+					{BUILDS.length > showedCardsCount && (
 						<div className={styles.showMoreButton}>
 							<ButtonSM isMobileFull={true} onClick={handleShowMoreClick}>
 								Show more
--- src/pages/MainPage/components/BuildCard/BuildCard.js
+++ src/pages/MainPage/components/BuildCard/BuildCard.js
@@ -1,30 +1,32 @@
 import React from 'react';
 import classNames from 'classnames';
+import {formatDate} from '@/utils/formatDate';
 
 import styles from './BuildCard.module.scss';
 
-const BuildCard = ({status}) => {
+const BuildCard = ({build}) => {
+	const {status, number, message, branch, hash, author, date, duration} = build;
 	return (
 		<div className={classNames(styles.buildCard, styles.build)}>
 			<div className={styles.info}>
 				<div className={styles.description}>
-					<span className={classNames(styles.number, styles[status])}>#1234</span>
-					<p className={styles.commitMessage}>add documentation for postgres scaler</p>
+					<span className={classNames(styles.number, styles[status])}>#{number}</span>
+					<p className={styles.commitMessage}>{message}</p>
 				</div>
 
 				<div className={styles.additionalInfo}>
 					<div className={styles.branch}>
 						<p className={styles.branchName}>
-							master <span className={styles.commitHash}>9c5s5d</span>
+							{branch} <span className={styles.commitHash}>{hash}</span>
 						</p>
 					</div>
-					<p className={styles.commitAuthor}>Philip Kirkorov</p>
+					<p className={styles.commitAuthor}>{author}</p>
 				</div>
 			</div>
 
 			<div className={styles.time}>
-				<p className={styles.date}>21 янв, 03:06</p>
-				<p className={styles.duration}>1 ч 20 мин</p>
+				<p className={styles.date}>{formatDate(date)}</p>
+				<p className={styles.duration}>{duration}</p>
 			</div>
 		</div>
 	);
